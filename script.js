3dca1230bfac3380e47ff4c05c3cafe4b8a8a276
David Connell <davecon88@gmail.com>
2020-09-30 18:51:41 +0930

    latest updates

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 $(document).ready(function  (){
+    var searchedCountries=[]
 
-    
-    
+    for (i=1;i<localStorage.length+1; i++){
+        searchedCountries[i]=localStorage.getItem(i)
+     
+        var searches = $("<li>", {id: i+"history"});
+       
+        $('#searchHistory').append(searches)
+        $("#"+i+"history").html(searchedCountries[i])
+  } 
+  
+  $('#country').keypress(function (e) {
+    var key = e.which;
+    if(key == 13){
+        Covid()
+    }})
+ 
     $('#search').click(function () {
-      // var APIkey="5cf9dfd5-3449-485e-b5ae-70a60e997864"
+        Covid()})
+        
+      function Covid() {
        var country = $("#country"). val();
+            k=localStorage.length
+            localStorage.setItem(k+1,country)
+            searchedCountries=[]
+            for (i=1;i<localStorage.length+1; i++){
+                searchedCountries[i]=localStorage.getItem(i)
+             
+                var searches = $("<li>", {id: i+"history"});
+               
+                $('#searchHistory').append(searches)
+                $("#"+i+"history").html(searchedCountries[i])
+          } 
+
+
        console.log(country)
        var today = moment.utc(moment().format()).format("YYYY-MM-DD")
        recentUpdate =  moment.utc(today).subtract(2,'d').format("YYYY-MM-DD") + "T00:00:00Z"
@@ -25,24 +54,41 @@ $(document).ready(function  (){
        $("#last_seven"). html(difference)
        $("#total_cases"). html(todayCases)
        
-       if (difference<2000) {
-         APIkey = "15c78bc522dec70681f7198c075cca56"
-        $.ajax({
-          url: "https://rapidapi.com/squawk7000/api/aerodatabox",
-          method: "GET"
-        }).then(function(response11) {
-       console.log(response11)
-        })
-       }
-      else {
-  return
-  
-  
-      }
-      })
-       
-      })
-  
-  
-    })
 
+    })
+  
+  
+ 
+      }
+
+      $('#mycity').keypress(function (e) {
+        var key = e.which;
+        if(key == 13){
+           storeUTC()
+        }})
+     
+
+        // $('#search').click(function () {
+        //     Covid()})
+  function storeUTC() {
+    var APIkeytimezone = "9652044f0e85425c9a02a36ec01ddd01&tz"
+    var myCity=$("#mycity"). val();
+    myCity=myCity.charAt(0).toUpperCase()+myCity.slice(1)
+   
+    k=localStorage.length
+    var myCountry=localStorage.getItem(k)
+    myCountry=myCountry.charAt(0).toUpperCase()+myCountry.slice(1)
+    thisCityUrl= "https://api.ipgeolocation.io/timezone?apiKey=" +APIkeytimezone+"="+ myCountry + "/" + myCity
+    
+    $.ajax({
+        url: thisCityUrl,
+        method: "GET",
+      }).then(function(response) {
+        myCityTime=response.timezone_offset
+   
+  })
+console.log(myCityTime)
+}
+    
+
+})
